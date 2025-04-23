05dd79347f269e7c37669cf706da420845ed1c20
Alonso Astroza Tagle <alonsoastroza@gmail.com>
2025-04-23 15:59:20 -0400

    chore: vector store id

--- config/constants.ts
+++ config/constants.ts
@@ -24,4 +24,4 @@ Hi, I'm ${AGENT_NAME}, your support representative. How can I help you today?
 
 // Replace with the vector store ID you get after initializing the vector store
 // Go to /init_vs to initialize the vector store with the demo knowledge base
-export const VECTOR_STORE_ID = "<vector_store_id>";
+export const VECTOR_STORE_ID = "vs_68084f87b0d881918b464f4ebb2fb895";
